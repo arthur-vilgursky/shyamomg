fbf3fcce372a1f0f6994a7f3af2cc150074138be
Artur Vilihurskyi <arthur.vilgursky@mirko.in.ua>
2023-05-22 18:53:18 +0300

    added tailwindCSS

--- www/webpack.mix.js
+++ www/webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+    .version()
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/css/main.scss', 'public/css')
+;
